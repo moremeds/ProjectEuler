2ce7d25d997d0a5a4aa15546c30e4b0454caf5eb
Chenxi Li <eason.li@me.com>
2016-06-02 00:05:16 +0100

    largest prime factor

---
+++ src/main/java/LargestPrimeFactor.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * ProjectEuler
+ * <p>
+ * Created by chenxili on 01/06/2016.
+ */
+public class LargestPrimeFactor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long number = scanner.nextInt();
+        List<Long> numSet = new LinkedList<>();
+
+        for (int i = 0; i < number; i++) {
+            numSet.add(scanner.nextLong());
+        }
+
+        for (long num : numSet) {
+            System.out.println(largestPrimeFactor(num));
+        }
+
+    }
+
+    public static long largestPrimeFactor(long n) {
+        int d = 2;
+        while (d <= n / d) {
+            if (n % d == 0)
+                n /= d;
+            else
+                d += 1;
+        }
+        return n;
+    }
+
+}
